21f3a74c03157a901d7b5b6eccbffe4b0c01fe3f
Anjo Tadena <tadena.anjo@gmail.com>
2022-01-16 21:11:44 +0800

    Basic http server

--- main.go
+++ main.go
@@ -5,16 +5,18 @@ import (
 	"net/http"
 )
 
+func Home(w http.ResponseWriter, r *http.Request) {
+	n, err := fmt.Fprintf(w, "Hello, World!")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Bytes written: %d", n))
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, World!")
-
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Println(fmt.Sprintf("Bytes written: %d", n))
-	})
+	http.HandleFunc("/", Home)
 
 	http.ListenAndServe(":5000", nil)
 }
