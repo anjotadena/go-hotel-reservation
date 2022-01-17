f6466bc4c695d6f4e11918211e448bcaf6c4aced
Anjo Tadena <tadena.anjo@gmail.com>
2022-01-17 23:54:09 +0800

    Use appconfig

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/anjotadena/projectX/pkg/config"
 	"github.com/anjotadena/projectX/pkg/handlers"
+	"github.com/anjotadena/projectX/pkg/render"
 )
 
 const portNumber = ":8000"
 
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+
+	if err != nil {
+		log.Fatal("Cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+
 	http.HandleFunc("/", handlers.Home)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
