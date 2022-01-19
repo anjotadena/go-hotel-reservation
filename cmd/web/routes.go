9ed4e4c51b339ee9489667ce3381e792b915f341
Anjo Tadena <tadena.anjo@gmail.com>
2022-01-20 06:32:46 +0800

    Implement file server to handle static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -23,5 +23,9 @@ func routes(app *config.AppConfig) http.Handler {
 	r.Get("/", handlers.Repo.Home)
 	r.Get("/about", handlers.Repo.About)
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+
+	r.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return r
 }
